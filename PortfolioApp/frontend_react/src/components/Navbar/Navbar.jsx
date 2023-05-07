ba36f4caa49aaf67b8b034dedbb9d5416ec80933
Hristo Yonkov <hristopturs@gmail.com>
2023-05-07 13:19:11 +0300

    add a navbar links

--- PortfolioApp/frontend_react/src/components/Navbar/Navbar.jsx
+++ PortfolioApp/frontend_react/src/components/Navbar/Navbar.jsx
@@ -5,10 +5,18 @@ import { images } from '../../constants';
 
 const Navbar = () => {
     return (
-        <nav>
+        <nav className='app__navbar'>
             <div>
                 <img src={images.logo} alt="logo" />
             </div>
+            <ul>
+                {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+                    <li key={`link-${item}`}>
+                        <div />
+                        <a href={`#${item}`}>{item}</a>
+                    </li>
+                ))}
+            </ul>
         </nav>
     )
 }
