7394a7f2b15539470d844c40f4a444469b85de11
Hristo Yonkov <hristopturs@gmail.com>
2023-05-08 13:21:03 +0300

    state and functionallity for mobile devices on navbar added

--- PortfolioApp/frontend_react/src/components/Navbar/Navbar.jsx
+++ PortfolioApp/frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { HiMenuAlt4, HiX } from 'react-icons/hi';
+import { motion } from 'framer-motion';
 
 import './Navbar.scss';
 import { images } from '../../constants';
 
 const Navbar = () => {
+    const [toggle, setToggle] = useState(false);
+
     return (
         <nav className='app__navbar'>
             <div className='app__navbar-logo'>
@@ -17,6 +21,25 @@ const Navbar = () => {
                     </li>
                 ))}
             </ul>
+
+            <div className="app__navbar-menu">
+                <HiMenuAlt4 onClick={() => setToggle(true)} />
+
+                {toggle && (
+                    <motion.div
+                        whileInView={{ x: [300, 0] }}
+                        transition={{ duration: 0.85, ease: 'easeOut' }}
+                    >
+                        <HiX onClick={() => setToggle(false)} />
+                        {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+                            <li key={item}>
+                                <div />
+                                <a href={`#${item}`} onClick={() => setToggle(false)}>{item}</a>
+                            </li>
+                        ))}
+                    </motion.div>
+                )}
+            </div>
         </nav>
     )
 }
