32e6e666316f7fa680f8356d154457b50612de25
Hristo Yonkov <hristopturs@gmail.com>
2023-05-08 15:12:06 +0300

    add header welcome text and animations

--- PortfolioApp/frontend_react/src/container/Header/Header.jsx
+++ PortfolioApp/frontend_react/src/container/Header/Header.jsx
@@ -1,11 +1,42 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 
+import { images } from '../../constants';
 import './Header.scss';
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
+    return (
+        <div className='app__header app__flex'>
+            <motion.div
+                whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+                transition={{ duration: 0.5 }}
+                className="app__header-info"
+            >
+                <div className='app__header-badge'>
+                    <div className='badge-cmp app__flex'>
+                        <span>👋</span>
+                        <div style={{ marginLeft: 20 }}>
+                            <p className='p-text'>Hello, Iam</p>
+                            <h1 className='head-text'>Hristo</h1>
+                        </div>
+                    </div>
+
+                    <div className='tag-cmp app__flex'>
+                        <p className='p-text'>Web Developer</p>
+                        <p className='p-text'>Junior</p>
+                    </div>
+                </div>
+            </motion.div>
+
+            <motion.div>
+
+            </motion.div>
+
+            <motion.div>
+
+            </motion.div>
+        </div>
+    )
 }
 
 export default Header;
