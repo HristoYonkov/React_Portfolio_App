833d8075e3acd28aff07afe7df0023e31eaf324e
Hristo Yonkov <hristopturs@gmail.com>
2023-05-08 12:40:46 +0300

    add styles on navbar and logo

--- PortfolioApp/frontend_react/src/components/Navbar/Navbar.jsx
+++ PortfolioApp/frontend_react/src/components/Navbar/Navbar.jsx
@@ -6,12 +6,12 @@ import { images } from '../../constants';
 const Navbar = () => {
     return (
         <nav className='app__navbar'>
-            <div>
+            <div className='app__navbar-logo'>
                 <img src={images.logo} alt="logo" />
             </div>
-            <ul>
+            <ul className='app__navbar-links'>
                 {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
-                    <li key={`link-${item}`}>
+                    <li key={`link-${item}`} className='app__flex p-text'>
                         <div />
                         <a href={`#${item}`}>{item}</a>
                     </li>
