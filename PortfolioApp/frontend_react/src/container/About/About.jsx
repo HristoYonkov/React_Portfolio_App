9c719cd5cfc60fba40f9b2de954a0c5b44281d7c
Hristo Yonkov <hristopturs@gmail.com>
2023-05-09 17:56:37 +0300

    fixed spacing on heading

--- PortfolioApp/frontend_react/src/container/About/About.jsx
+++ PortfolioApp/frontend_react/src/container/About/About.jsx
@@ -15,13 +15,7 @@ const abouts = [
 const About = () => {
     return (
         <>
-            <h2 className='head-text'>
-                I Know That
-                <span>Good Design</span>
-                <br />
-                means
-                <span>Good Business</span>
-            </h2>
+            <h2 className='head-text'>I Know That <span>Good Apps</span><br />means <span>Good Business</span></h2>
 
             <div className='app__profiles'>
                 {abouts.map((about, index) => (
