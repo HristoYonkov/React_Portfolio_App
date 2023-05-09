fb289d905cf35097b8a9e3376b8bf103262c7e91
Hristo Yonkov <hristopturs@gmail.com>
2023-05-09 15:42:27 +0300

    add header text into about section

--- PortfolioApp/frontend_react/src/container/About/About.jsx
+++ PortfolioApp/frontend_react/src/container/About/About.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
+
 
 import './About.scss';
 
 const About = () => {
-  return (
-    <div>About</div>
-  )
+    return (
+        <>
+            <h2 className='head-text'>
+                I Know That
+                <span>Good Design</span>
+                <br />
+                means
+                <span>Good Business</span>
+            </h2>
+        </>
+    )
 }
 
 export default About;
