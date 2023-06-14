f6a0311bf94bad1c628384bec499ffe4f5c500a5
Hristo Yonkov <hristopturs@gmail.com>
2023-06-14 17:55:23 +0300

    add social media comp.

---
+++ PortfolioApp/frontend_react/src/components/SocialMedia.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { FaGithubSquare, FaLinkedin, FaFacebook, FaGithub } from 'react-icons/fa';
+const SocialMedia = () => {
+    return (
+        <div className='app__social'>
+            <div>
+                <FaGithubSquare />
+            </div>
+            <div>
+                <FaLinkedin />
+            </div>
+            <div>
+                <FaFacebook />
+            </div>
+        </div>
+    )
+}
+
+export default SocialMedia
