790638d3d98cfb7da3ccf43486fbf96d9f18de7e
Hristo Yonkov <hristopturs@gmail.com>
2023-06-14 17:56:53 +0300

    import dot hoc to about page

--- PortfolioApp/frontend_react/src/container/About/About.jsx
+++ PortfolioApp/frontend_react/src/container/About/About.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 
 import './About.scss';
-import { images } from '../../constants';
+import { AppWrap } from '../../wrapper';
 import { urlFor, client } from '../../client';
 
 
@@ -38,4 +38,4 @@ const About = () => {
     )
 }
 
-export default About;
+export default AppWrap(About, 'about');
