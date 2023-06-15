f9ffabae2177d2951586f7d40cd09bf6b6e600c9
Hristo Yonkov <hristopturs@gmail.com>
2023-06-15 18:39:59 +0300

    fix the problem with css

--- PortfolioApp/frontend_react/src/container/Work/Work.jsx
+++ PortfolioApp/frontend_react/src/container/Work/Work.jsx
@@ -8,7 +8,7 @@ import './Work.scss';
 
 const Work = () => {
   const [activeFilter, setActiveFilter] = useState('All');
-  const [animateCard, setAnimateCard] = useState({y: 0, opacity: 1});
+  const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 });
   const [works, setWorks] = useState([]);
   const [filterWork, setFilterWork] = useState([]);
 
@@ -20,7 +20,7 @@ const Work = () => {
         setFilterWork(data);
       })
   }, []);
-  
+
   const handleWorkFilter = (item) => {
 
   }
@@ -38,28 +38,28 @@ const Work = () => {
             {item}
           </div>
         ))}
+      </div>
+      <motion.div
+        animate={animateCard}
+        transition={{ duration: 0.5, delayChildren: 0.5 }}
+        className='app__work-portfolio'
 
-        <motion.div
-          animate={animateCard}
-          transition={{duration: 0.5, delayChildren: 0.5}}
-          className='app__work-portfolio'
-
-        >
+      >
         {filterWork.map((work, index) => (
           <div className='app__work-item app__flex' key={index}>
             <div className='app__work-img app_flex'>
               <img src={urlFor(work.imgUrl)} alt={work.name} />
 
               <motion.div
-                whileHover={{opacity: [0, 1]}}
-                transition={{duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5}}
+                whileHover={{ opacity: [0, 1] }}
+                transition={{ duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5 }}
                 className='app__work-hover app__flex'
               >
                 <a href={work.projectLink} target='_blank' rel='noreferrer'>
                   <motion.div
-                    whileInView={{scale: [0, 1]}}
-                    whileHover={{scale: [1, 0.9]}}
-                    transition={{duration: 0.25}}
+                    whileInView={{ scale: [0, 1] }}
+                    whileHover={{ scale: [1, 0.9] }}
+                    transition={{ duration: 0.25 }}
                     className='app__flex'
                   >
                     <AiFillEye />
@@ -67,9 +67,9 @@ const Work = () => {
                 </a>
                 <a href={work.codeLink} target='_blank' rel='noreferrer'>
                   <motion.div
-                    whileInView={{scale: [0, 1]}}
-                    whileHover={{scale: [1, 0.9]}}
-                    transition={{duration: 0.25}}
+                    whileInView={{ scale: [0, 1] }}
+                    whileHover={{ scale: [1, 0.9] }}
+                    transition={{ duration: 0.25 }}
                     className='app__flex'
                   >
                     <AiFillGithub />
@@ -80,7 +80,7 @@ const Work = () => {
 
             <div className='app__work-content app__flex'>
               <h4 className='bold-text'>{work.title}</h4>
-              <p className="p-text" style={{marginTop: 10}}>{work.descrption}</p>
+              <p className="p-text" style={{ marginTop: 10 }}>{work.description}</p>
 
               <div className='app__work-tag app__flex'>
                 <p className='p-text'>{work.tags[0]}</p>
@@ -88,8 +88,7 @@ const Work = () => {
             </div>
           </div>
         ))}
-        </motion.div>
-      </div>
+      </motion.div>
     </>
   )
 }
