200736445165aba7e34640c5bbc087ef6405c702
Hristo Yonkov <hristopturs@gmail.com>
2023-06-15 12:04:40 +0300

    "work" section start

--- PortfolioApp/frontend_react/src/container/Work/Work.jsx
+++ PortfolioApp/frontend_react/src/container/Work/Work.jsx
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { AiFillEye, AiFillGithub } from 'react-icons/ai';
+import { motion } from 'framer-motion';
 
+import { AppWrap } from '../../wrapper';
+import { urlFor, client } from '../../client';
 import './Work.scss';
 
 const Work = () => {
+  const [activeFilter, setActiveFilter] = useState('All');
+
+  const handleWorkFilter = (item) => {
+
+  }
+
   return (
-    <div>Work</div>
+    <>
+      <h2 className='head-text'>My Creative <span>Portfolio</span> section</h2>
+
+      <div className="app__work-filter">
+        {['UI/UX', 'Web App', 'Mobile App', 'React JS', 'All'].map((item, index) => (
+          <div
+            key={index}
+            onClick={() => handleWorkFilter(item)}
+            className={`app__work-filter-item app__flex p-text ${activeFilter === item ? 'item-active' : ''}`}
+          >
+            {item}
+          </div>
+        ))}
+      </div>
+    </>
   )
 }
 
