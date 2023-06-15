094dc09fe4816b7ac5c8fdce19471b456c46d136
Hristo Yonkov <hristopturs@gmail.com>
2023-06-15 14:11:09 +0300

    add content for work section

--- PortfolioApp/frontend_react/src/container/Work/Work.jsx
+++ PortfolioApp/frontend_react/src/container/Work/Work.jsx
@@ -19,7 +19,6 @@ const Work = () => {
         setWorks(data);
         setFilterWork(data);
       })
-      console.log(filterWork);
   }, []);
   
   const handleWorkFilter = (item) => {
@@ -51,15 +50,41 @@ const Work = () => {
             <div className='app__work-img app_flex'>
               <img src={urlFor(work.imgUrl)} alt={work.name} />
 
-              {/* <motion.div
+              <motion.div
                 whileHover={{opacity: [0, 1]}}
                 transition={{duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5}}
                 className='app__work-hover app__flex'
               >
                 <a href={work.projectLink} target='_blank' rel='noreferrer'>
-
+                  <motion.div
+                    whileInView={{scale: [0, 1]}}
+                    whileHover={{scale: [1, 0.9]}}
+                    transition={{duration: 0.25}}
+                    className='app__flex'
+                  >
+                    <AiFillEye />
+                  </motion.div>
                 </a>
-              </motion.div> */}
+                <a href={work.codeLink} target='_blank' rel='noreferrer'>
+                  <motion.div
+                    whileInView={{scale: [0, 1]}}
+                    whileHover={{scale: [1, 0.9]}}
+                    transition={{duration: 0.25}}
+                    className='app__flex'
+                  >
+                    <AiFillGithub />
+                  </motion.div>
+                </a>
+              </motion.div>
+            </div>
+
+            <div className='app__work-content app__flex'>
+              <h4 className='bold-text'>{work.title}</h4>
+              <p className="p-text" style={{marginTop: 10}}>{work.descrption}</p>
+
+              <div className='app__work-tag app__flex'>
+                <p className='p-text'>{work.tags[0]}</p>
+              </div>
             </div>
           </div>
         ))}
@@ -69,4 +94,4 @@ const Work = () => {
   )
 }
 
-export default Work;
+export default AppWrap(Work, 'work');
