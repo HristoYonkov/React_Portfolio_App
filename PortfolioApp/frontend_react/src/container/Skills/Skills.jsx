f045a8ab04aaf37a6b8205f76a2e65a2529e38dc
Hristo Yonkov <hristopturs@gmail.com>
2023-06-18 18:09:54 +0300

    addd syling

--- PortfolioApp/frontend_react/src/container/Skills/Skills.jsx
+++ PortfolioApp/frontend_react/src/container/Skills/Skills.jsx
@@ -24,27 +24,27 @@ const Skills = () => {
       .then((data) => {
         setSkills(data);
       })
-  }, []);
-
+    }, []);
+    
   return (
     <>
       <h2 className='head-text'>Skills & Experience</h2>
 
       <div className='app__skills-container'>
         <motion.div className='app__skills-list'>
-          {skills.map((skill) => {
+          {skills.map((skill) => (
             <motion.div
-              whileInView={{ opacity: [0, 1] }}
-              transition={{ duration: 0.5 }}
-              className='app__skills-item app__flex'
-              key={skill.name}
-            >
-              <div className='app__flex' style={{backgroundColor: skill.bgColor}}>
-                <img src={urlFor(skill.icon)} alt={skill.name} />
-              </div>
-              
-            </motion.div>
-          })}
+            whileInView={{ opacity: [0, 1] }}
+            transition={{ duration: 0.5 }}
+            className='app__skills-item app__flex'
+            key={skill.name}
+          >
+            <div className='app__flex' style={{backgroundColor: skill.bgColor}}>
+              <img src={urlFor(skill.icon)} alt={skill.name} />
+            </div>
+            <p className='p-text'>{skill.name}</p>
+          </motion.div>
+          ))}
         </motion.div>
       </div>
     </>
