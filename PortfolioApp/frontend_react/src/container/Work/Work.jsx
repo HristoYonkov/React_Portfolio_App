393f5acbd9e34577bf1365500517a2b3b3be9ac6
Hristo Yonkov <hristopturs@gmail.com>
2023-06-18 11:59:26 +0300

    add filtering functionality to the work section

--- PortfolioApp/frontend_react/src/container/Work/Work.jsx
+++ PortfolioApp/frontend_react/src/container/Work/Work.jsx
@@ -22,7 +22,18 @@ const Work = () => {
   }, []);
 
   const handleWorkFilter = (item) => {
+    setActiveFilter(item);
+    setAnimateCard({y: 100, opacity: 0});
 
+    setTimeout(() => {
+    setAnimateCard([{y: 0, opacity: 1}])
+
+      if (item === 'All') {
+        setFilterWork(works);
+      } else {
+        setFilterWork(works.filter((work) => work.tags.includes(item)));
+      }
+    }, 500);
   }
   return (
     <>
@@ -39,11 +50,11 @@ const Work = () => {
           </div>
         ))}
       </div>
+      
       <motion.div
         animate={animateCard}
         transition={{ duration: 0.5, delayChildren: 0.5 }}
         className='app__work-portfolio'
-
       >
         {filterWork.map((work, index) => (
           <div className='app__work-item app__flex' key={index}>
