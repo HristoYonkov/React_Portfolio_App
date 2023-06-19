ad7e082e9fbaf5edf98ec34868535b9b65837a75
Hristo Yonkov <hristopturs@gmail.com>
2023-06-19 18:00:25 +0300

    Update Skills.jsx

--- PortfolioApp/frontend_react/src/container/Skills/Skills.jsx
+++ PortfolioApp/frontend_react/src/container/Skills/Skills.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
-import { Tooltip as ReactTooltip } from 'react-tooltip';
+import { Tooltip } from 'react-tooltip';
 
 import { AppWrap } from '../../wrapper';
 import { urlFor, client } from '../../client';
@@ -69,14 +69,14 @@ const Skills = () => {
                       <h4 className='bold-text'>{work.name}</h4>
                       <p className='p-text'>{work.company}</p>
                     </motion.div>
-                    <ReactTooltip
+                    <Tooltip
                       id={work.name}
                       effect='solid'
                       arrowColor='#fff'
                       className='skills-tooltip'
                     >
                       {work.desc}
-                    </ReactTooltip>
+                    </Tooltip>
                   </>
                 ))}
               </motion.div>
