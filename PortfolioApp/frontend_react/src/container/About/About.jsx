e0e877e3edbf49d583c1a641d0f92499d1bd025b
Hristo Yonkov <hristopturs@gmail.com>
2023-06-19 18:37:00 +0300

    add wrapper to about

--- PortfolioApp/frontend_react/src/container/About/About.jsx
+++ PortfolioApp/frontend_react/src/container/About/About.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 
 import './About.scss';
-import { AppWrap } from '../../wrapper';
+import { AppWrap, MotionWrap } from '../../wrapper';
 import { urlFor, client } from '../../client';
 
 
@@ -38,4 +38,8 @@ const About = () => {
     )
 }
 
-export default AppWrap(About, 'about');
+export default AppWrap(
+    MotionWrap(About, 'app__about'),
+    'about',
+    'app__whitebg'
+);
