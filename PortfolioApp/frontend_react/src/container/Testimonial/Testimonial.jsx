d708b5547757690566b635f0c81fb576b6411685
Hristo Yonkov <hristopturs@gmail.com>
2023-06-22 11:36:32 +0300

    prepare imports for work

--- PortfolioApp/frontend_react/src/container/Testimonial/Testimonial.jsx
+++ PortfolioApp/frontend_react/src/container/Testimonial/Testimonial.jsx
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
+import { HiChevronLeft, HiChevronRight } from 'react-icons/hi';
 
+import { AppWrap, MotionWrap } from '../../wrapper';
+import { urlFor, client } from '../../client';
 import './Testimonial.scss';
 
 const Testimonial = () => {
+  const [brands, setBrands] = useState([]);
+  const [testimonials, setTestimonials] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const brandsQuery = '*[_type == "brands"]';
+    const testimonialsQuery = '*[_type == "testimonials"]';
+
+    client.fetch(brandsQuery)
+      .then((data) => {
+        setBrands(data);
+      })
+
+    client.fetch(testimonialsQuery)
+      .then((data) => {
+        setTestimonials(data);
+      })
+  }, []);
+
   return (
     <div>Testimonial</div>
   )
 }
 
-export default Testimonial;
+export default AppWrap(
+  MotionWrap(Testimonial, 'app__testimonial'),
+  'testimonial',
+  'app__primarybg'
+);
