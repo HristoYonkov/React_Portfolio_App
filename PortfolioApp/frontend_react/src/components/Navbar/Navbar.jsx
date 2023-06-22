f7ef96afc07f9687e83ebb6896e0485216b8427d
Hristo Yonkov <hristopturs@gmail.com>
2023-06-22 11:34:14 +0300

    add a dot div above the links

--- PortfolioApp/frontend_react/src/components/Navbar/Navbar.jsx
+++ PortfolioApp/frontend_react/src/components/Navbar/Navbar.jsx
@@ -16,6 +16,7 @@ const Navbar = () => {
             <ul className='app__navbar-links'>
                 {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
                     <li key={`link-${item}`} className='app__flex p-text'>
+                        <div></div>
                         <a href={`#${item}`}>{item}</a>
                     </li>
                 ))}
