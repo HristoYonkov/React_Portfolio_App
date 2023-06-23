21758ba4ca39ab68195344d55f8a3e2941642736
Hristo Yonkov <hristopturs@gmail.com>
2023-06-23 17:04:21 +0300

    Update Footer.jsx

--- PortfolioApp/frontend_react/src/container/Footer/Footer.jsx
+++ PortfolioApp/frontend_react/src/container/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-import images from '../../constants';
+import { images } from '../../constants';
 import { AppWrap, MotionWrap } from '../../wrapper';
 import { client } from '../../client';
 
@@ -8,7 +8,20 @@ import './Footer.scss';
 
 const Footer = () => {
   return (
-    <h2 className='head-text'>Take a coffe & chat with me</h2>
+    <>
+      <h2 className='head-text'>Take a coffe & chat with me</h2>
+
+      <div className='app__footer-cards'>
+        <div className='app__footer-card'>
+          <img src={images.email} alt="email" />
+          <a href="mailto:hristopturs@gmail.com" className='p-text'>hristopturs@gmail.com</a>
+        </div>
+        <div className='app__footer-card'>
+          <img src={images.email} alt="mobile" />
+          <a href="tel: +359 (899) 567-539" className='p-text'>+359 (899) 567-539</a>
+        </div>
+      </div>
+    </>
   )
 }
 
