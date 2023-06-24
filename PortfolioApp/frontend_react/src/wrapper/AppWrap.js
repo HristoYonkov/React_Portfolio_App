f5d65e62f9e4641341d1e25a1cb4ab5979d1e8d7
Hristo Yonkov <hristopturs@gmail.com>
2023-06-24 16:55:54 +0300

    Update AppWrap.js

--- PortfolioApp/frontend_react/src/wrapper/AppWrap.js
+++ PortfolioApp/frontend_react/src/wrapper/AppWrap.js
@@ -10,7 +10,7 @@ const AppWrap = (Component, idName, classNames) => function HOC() {
                 <Component />
 
                 <div className='copyright'>
-                    <p className="p-text">@2023 HRISTO</p>
+                    <p className="p-text">@2023 HRISTO YONKOV</p>
                     <p className="p-text">All Rights Reserved</p>
                 </div>
             </div>
