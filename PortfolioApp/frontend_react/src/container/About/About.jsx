8ce15b420a7ed949adb40bf1f0fd05e793bd6bda
Hristo Yonkov <hristopturs@gmail.com>
2023-06-26 18:17:25 +0300

    Update About.jsx

--- PortfolioApp/frontend_react/src/container/About/About.jsx
+++ PortfolioApp/frontend_react/src/container/About/About.jsx
@@ -23,7 +23,7 @@ const About = () => {
                 {abouts.map((about, index) => (
                     <motion.div
                         whileInView={{ opacity: 1 }}
-                        whileHover={{ scale: 1.1 }}
+                        whileHover={{ scale: 1.2 }}
                         transition={{ duration: 0.5, type: 'tween' }}
                         className='app__profile-item'
                         key={about.title + index}
